6078d06423a4506c68a86e4e6fbe6b751591ef96
boberstarosta <boberstarosta@gmail.com>
2021-08-27 15:21:20 +0200

    remove __pycache__

--- rail/tracks.py
+++ rail/tracks.py
@@ -1,6 +1,20 @@
-import networkx as nx
+from rail.graph import Graph
 
 
 class Node:
     def __init__(self, position):
         self._position = position
+
+    @property
+    def position(self):
+        return self._position
+
+
+class TrackManager:
+    def __init__(self):
+        self.graph = Graph()
+    
+    def add_track(self, start, end):
+        node1 = Node(start)
+        node2 = Node(end)
+        self.graph.add_edge(node1, node2)
